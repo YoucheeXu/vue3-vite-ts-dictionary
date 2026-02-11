6bc876b4009c7653cfdfb14705c66a47e7202d56
Youchee Xu <youchi.xu@live.com>
2026-02-11 22:01:03 +0800

    chore: default to route recite

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,7 +11,7 @@ const router = createRouter({
     routes: [
         {
             path: "/",
-            redirect: "/main",
+            redirect: "/recite",
         },
         {
             path: "/main",
