0dd685240c3133dd3adf6306301816c2438f8d72
Youchee <youchi.xu@live.com>
2026-04-13 09:30:05 +0800

    feat: add `debounce`

--- src/utilities/utilities.ts
+++ src/utilities/utilities.ts
@@ -1,3 +1,5 @@
+type TimerId = ReturnType<typeof setInterval>;
+
 // Throttle function
 export function throttle (fn: Function, delay: number) {
     let lastCall = 0;
@@ -9,3 +11,34 @@ export function throttle (fn: Function, delay: number) {
         }
     };
 };
+
+// export function debounce2(fn: Function, duration: number = 300){
+//     let timerId: TimerId;
+//     return function (...args){
+//         clearTimeout(timerId);
+//         timerId = setTimeout(() => {
+//             fn.call(this, ...args);
+//         }, duration);
+//     };
+// }
+
+/**
+ * Type-safe debounce function
+ * @param fn - The function to debounce
+ * @param duration - Debounce delay in ms (default: 300)
+ * @returns Debounced function with preserved types
+ */
+export function debounce<F extends (...args: any[]) => any>(
+    fn: F,
+    duration: number = 300
+) {
+    let timerId: TimerId;
+
+    // Annotate `this` type + type the rest parameters
+    return function (this: ThisParameterType<F>, ...args: Parameters<F>): void {
+            clearTimeout(timerId);
+            timerId = setTimeout(() => {
+            fn.call(this, ...args);
+        }, duration);
+    };
+}
